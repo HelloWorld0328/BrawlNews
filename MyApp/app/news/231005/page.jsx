939399e88d3cfeddb5df35def332a70f029488d0
HelloWorld0328 <rdy00328@gmail.com>
2023-10-06 21:37:16 +0900

    news update

---
+++ MyApp/app/news/231005/page.jsx
@@ -0,0 +1,14 @@
+export default function news231005(){
+    return(
+        <main>
+            <h1 id="title">브롤스타즈 웹사이트 재작 완료</h1>
+            <h2 id="subtitle">브롤스타즈 웹사이트가 재작되었습니다!</h2><br />
+            <h2 id="sodyd">
+                아직 CSS(스타일),이쁜도메인등 많이 추가 못했습니다. <br />
+                뉴스도 아직 추가 많이안했어요. <br /><br />
+                링크:<a href="https://brawl-news.vercel.app/">https://brawl-news.vercel.app/</a>
+
+            </h2>
+        </main>
+    )
+}
