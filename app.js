804420ef8c473ecb0f02ce02ea7345cf873d9596
HelloWorld0328 <rdy00328@gmail.com>
2023-10-21 12:11:27 +0900

    post update

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
-//시스템 변수선언
-const express=require('express')
-const fs = require('fs')
-const app=express()
-const port=3000
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
-//json읽기
-function readJson(){
-    var a=fs.readFileSync(__dirname+"/posts.json","utf-8")
-    return a
+app.use(bodyParser.json()); // JSON 데이터 파싱
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// JSON 파일 경로
+const jsonFilePath = __dirname + "/posts.json";
+
+// JSON 파일을 읽어오는 함수
+function readJson() {
+  try {
+    const data = fs.readFileSync(jsonFilePath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
 }
-function writeJson(chg){
-    var read=fs.readFileSync(__dirname+"/posts.json","utf-8")
-    var readData=JSON.parse(read)
-    readData.append(chg)
-    read=JSON.stringify(readData)
-    return(read)
-    
+
+// JSON 파일에 데이터를 저장하는 함수
+function writeJson(data) {
+  fs.writeFileSync(jsonFilePath, JSON.stringify(data, null, 2), "utf-8");
 }
-//라우팅
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/index.html")
-})
 
+// 루트 경로
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-//서버
-app.get('/posts',(req,res)=>{
-    // res.send(JSON.stringify([{title:"테스트",content:"테스트 글입니다."}]))
-    res.send(readJson())
-})
-app.post('/upload',(req,res)=>{
-    console.log("req:"+req)
-    res.send(JSON.parse(req))
-})
+// 모든 포스트 가져오기
+app.get('/posts', (req, res) => {
+  const posts = readJson();
+  res.json(posts);
+});
 
-//리스닝
-app.listen(port,()=>{console.log(`listening on ${port}!`)})
+// 새로운 포스트 추가
+app.post('/upload', (req, res) => {
+  const reqdata = req.body;
+  console.log(reqdata);
+
+  // JSON 파일에서 데이터 읽기
+  const posts = readJson();
+
+  // 새로운 데이터 추가
+  posts.push(reqdata);
+
+  // JSON 파일에 데이터 저장
+  writeJson(posts);
+
+  res.json({ message: '데이터가 성공적으로 추가되었습니다.' });
+});
+
+app.listen(port, () => {
+  console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
+});
